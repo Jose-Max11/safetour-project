faabf53e420dda6116a52c372ead29d700755ed7
Jose-Max11 <2312075@nec.edu.in>
2026-03-09 22:56:58 +0530

    this is new Change

--- safetour/backend/server.js
+++ safetour/backend/server.js
@@ -6,7 +6,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5001;
 
 // ─── Security Middleware ─────────────────────────────────────────────────────
 app.use(helmet());
